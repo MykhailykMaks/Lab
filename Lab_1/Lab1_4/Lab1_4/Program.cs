8240d4f1c9c9e1a71f0e148da1a1b81f5a23ee29
murzik <m.mykhailyk24@gmail.com>
2025-03-10 18:25:28 +0200

    Додав перше завдання 2 лаби

--- Lab_1/Lab1_4/Lab1_4/Program.cs
+++ Lab_1/Lab1_4/Lab1_4/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] infoAboutFlight = File.ReadAllLines("Lab1_4.txt");
+            double xFuel = double.Parse(infoAboutFlight[0]);
+            double weight = double.Parse(infoAboutFlight[1]);
+            double AB = double.Parse(infoAboutFlight[2]);
+            double BC = double.Parse(infoAboutFlight[3]);
+            int n = 0;
+            if (weight > 0 && weight < 500)
+            {
+                n = 1;
+            }
+            else if (weight >= 500 && weight < 1000)
+            {
+                n = 4;
+            }
+            else if (weight >= 1000 && weight < 1500)
+            {
+                n = 7;
+            }
+            else if (weight >= 1500 && weight < 2000)
+            {
+                n = 9;
+            }
+            else
+            {
+                Console.WriteLine("Flight impossible");
+            }
+            double fuelForAB = AB * n;
+            double fuelForBC = BC * n;
+            if ((AB * n) <= xFuel)
+            {
+                xFuel -= fuelForAB;
+                Console.WriteLine("Flight to B is successful");
+                if(xFuel < fuelForBC)
+                {
+                    Console.WriteLine("We need a refuel");
+                    double minRefuel = fuelForBC - xFuel;
+                    xFuel += minRefuel;
+                    Console.WriteLine($"Minimal refuel for this flight:{minRefuel}");
+                }
+                if ((BC * n) <= xFuel)
+                {
+                    Console.WriteLine("Flight to C is successful");
+                }
+                else
+                {
+                    Console.WriteLine("Flight to C is unsuccessful!!");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Flight to B is unsuccessful!!");
+                return;
+            }
         }
     }
 }

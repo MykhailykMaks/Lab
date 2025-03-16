05e108f9221bdbdbe1d1989ebe0b0cb8e128c5f7
murzik <m.mykhailyk24@gmail.com>
2025-03-16 23:21:25 +0200

    Додав другу та частину третьої лаби

---
+++ Lab_2/Lab2_2/Lab2_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lab2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] xNumbers = { 2, 10, -10 };
+            double eps = 1e-6;
+            double S = 1.0;
+            double term = 1.0;
+            double ln2 = Math.Log(2);
+            int n = 1;
+            Console.WriteLine(" x\t S\t y(x) = 2^x\t Error");
+            for (int i = 0; i < xNumbers.Length; ++i)
+            {
+                double x = xNumbers[i];  
+                while (Math.Abs(term) >= eps)
+                {
+                     term *= (x * ln2) / n;
+                     S += term;
+                     n++;
+                }
+                double y = Math.Pow(2, x);
+                double error = Math.Abs(S - y);
+                Console.WriteLine($" {x}\t {S:F3}\t {y:F6}\t {error:E}");
+            }    
+        }
+    }
+}
---
+++ Lab_3/Lab3_1/Lan3_1/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Lan3_1
+{
+    internal class Program
+    {
+        static int MinNum(int[] Arr, int N)
+        {
+            int min = Arr[0];
+            for (int i = 1; i < N; ++i)
+            {
+                if (Arr[i] < min)
+                {
+                    min = Arr[i];
+                }
+            }
+            return min;
+        }
+
+        static int NegativeNum(int[] Arr, int N)
+        {
+            int negative = Arr[0];
+            for (int i = 0; i < N; ++i)
+            {
+                if (Arr[i] < 0)
+                {
+                    negative = Arr[i];
+                }
+            }
+            return negative;
+        }
+
+        static int MinIndex(int[] Arr, int N, int number)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static int NegativeIndex(int[] Arr, int N, int number)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                if (Arr[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        static void Main()
+        {
+            Random rand = new Random();
+            const int N = 40;
+            int[] Arr = new int[N];
+            for (int i = 0; i < N; ++i)
+            {
+                Arr[i] = rand.Next(-50, 50);
+            }
+            Console.WriteLine("Array:");
+            foreach (int value in Arr)
+            {
+                Console.Write(value.ToString().PadLeft(10));
+            }
+            Console.WriteLine();
+            int minNumber = MinNum(Arr, N);
+            Console.WriteLine($"\nMin. number: {minNumber}");
+            int negative = NegativeNum(Arr, N);
+            Console.WriteLine($"\nLast negative number: {negative}");
+            int minNumberIndex = MinIndex(Arr, N, minNumber);
+            Console.WriteLine($"\nMin. number index: {minNumberIndex}");
+            int negativIndex = NegativeIndex(Arr, N, negative);
+            Console.WriteLine($"\nLast negative number index: {negativIndex}");
+
+            int index1 = minNumberIndex;
+            int index2 = negativIndex;
+
+            int buf = Arr[index1];
+            Arr[index1] = Arr[index2];
+            Arr[index2] = buf;
+
+            Console.WriteLine("\nArray after swapping:");
+            foreach (int value in Arr)
+            {
+                Console.Write(value.ToString().PadLeft(10));
+            }
+        }
+    }
+
+}
+

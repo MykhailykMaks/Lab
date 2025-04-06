17ace693ee83d3ba25c8f37968d689c4c5b4717d
murzik <m.mykhailyk24@gmail.com>
2025-04-06 23:31:13 +0300

    Додав 4 лабу

---
+++ Lab_4/Lab4_1/Lab4_1(1)/Lab4_1(1)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lab4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "abcdefghijklmnopqrstuv18340";
+            Regex regex = new Regex(phrase);
+            Console.Write("Input a test phrase:");
+            string inputPhrase = Console.ReadLine();
+            if (regex.IsMatch(inputPhrase))
+            {
+                Console.WriteLine("phrase is correct");
+            }
+            else
+            {
+                Console.WriteLine("phrase is not correct");
+            }
+        }
+    }
+}

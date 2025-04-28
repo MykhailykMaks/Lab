f3d884fbb346bc6f4bed86b8685b6b9244c951f5
murzik <m.mykhailyk24@gmail.com>
2025-04-28 21:57:22 +0300

    6 лаба 1 завдання

---
+++ Lab_6/Lab_6(1)/Lab_6(1)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triada[] test = {Create.GetRandomTriada(), Create.GetRandomTriada(), Create.GetRandomTime(), Create.GetRandomTime() };
+            foreach (var item in test)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Objects after metods");
+            foreach (var item in test)
+            {
+                item.PlusOne();
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}

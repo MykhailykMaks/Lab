ae37d2394fab40e02cd357b1977d82122cc04035
murzik <m.mykhailyk24@gmail.com>
2025-05-25 18:22:03 +0300

    фул 8 лаба

---
+++ Lab_8/Lab_8(2)/Lab_8(2)/Create.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_8_2_
+{
+    class Create
+    {
+        static Random random = new Random();
+        static string[] colors = { "Red", "Blue", "Green", "Black", "White" };
+        static string[] manufacturers = { "Manufacturer1", "Manufacturer2", "Manufacturer3", "Manufacturer4" };
+        static int[] weights = { 1, 2, 3, 4 ,5 };
+        static int[] sizes = { 10, 20, 30, 40, 50 };    
+        static int[] capacity = {5, 10, 15, 20, 25 };
+        static string[] items = { "Item1", "Item2", "Item3", "Item4", "Item5" };
+        static int[] sizesItems = { 1, 2, 3, 4, 5 };
+        public static string GetRandomColor()
+        {
+            return colors[random.Next(colors.Length)];
+        }
+        public static string GetRandomManufacturer()
+        {
+            return manufacturers[random.Next(manufacturers.Length)];
+        }
+        public static int GetRandomWeight()
+        {
+            return weights[random.Next(weights.Length)];
+        }
+        public static int GetRandomSize()
+        {
+            return sizes[random.Next(sizes.Length)];
+        }
+        public static int GetRandomCapacity()
+        {
+            return capacity[random.Next(capacity.Length)];
+        }
+        public static string GetRandomItemName()
+        {
+            return items[random.Next(items.Length)];
+        }
+        public static int GetRandomItemSize()
+        {
+            return sizesItems[random.Next(sizesItems.Length)];
+        }
+        public static Items CreateItems()
+        {
+            return new Items(GetRandomItemName(), GetRandomItemSize());
+        }
+        public static Case CreateCase()
+        {
+            return new Case(GetRandomColor(), GetRandomManufacturer(), GetRandomWeight(), GetRandomSize(), new Items[0], GetRandomCapacity());
+        }
+    }
+}
---
+++ Lab_8/Lab_8(2)/Lab_8(2)/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab_8_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Items item1 = Create.CreateItems();
+            Items item2 = Create.CreateItems();
+            Items item3 = Create.CreateItems();
+            Items[] items = { item1, item2, item3 };
+            Case myCase = Create.CreateCase();
+            Console.WriteLine("Case and items:");
+            Console.WriteLine(myCase.ToString());
+            Console.WriteLine(item1.ToString());
+            Console.WriteLine(item2.ToString());
+            Console.WriteLine(item3.ToString());
+            Handler1 handler1 = new Handler1();
+            Handler2 handler2 = new Handler2();
+            myCase.ItemAdd += handler1.ItemsAddHandler;
+            myCase.ItemAdd += handler2.ItemDeleteHandler;
+            Console.WriteLine("Case after changing:");
+            myCase.AddItems(item1);
+            Console.WriteLine(myCase.ToString());
+            myCase.DeleteItems(item1);
+            Console.WriteLine(myCase.ToString());
+        }
+            
+    }
+}

5de0c49471c1d369b8aa37ebc05eb24de1072218
murzik <m.mykhailyk24@gmail.com>
2025-05-26 23:49:29 +0300

    9 лаба

---
+++ Lab_9/Lab_9(3)/Lab_9(3)/MethodsForSongs.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_3_
+{
+    class MethodsForSongs : Song
+    {
+        public static Song[] songs;
+        static MethodsForSongs()
+        {
+            songs = new Song[0];
+        }
+        public static void AddSong(Song song)
+        {
+            Array.Resize(ref songs, songs.Length + 1);
+            songs[songs.Length - 1] = song;
+        }
+        public static void DeleteSong(Song song)
+        {
+            Song[] newSongers = new Song[songs.Length - 1];
+            int index = Array.IndexOf(songs, song);
+            int j = 0;
+            if (index >= 0)
+            {
+                for (int i = 0; i < songs.Length; i++)
+                {
+                    if (i != index)
+                    {
+                        newSongers[j] = songs[i];
+                        j++;
+                    }
+                }
+            }
+            songs = newSongers;
+        }
+        public void NewInfoSong(string newNameSong = "", string newPIBAvtor = "", string newComposer = "", int newYear = 0, Songer[]? newSongers = null)
+        {
+            nameSong = newNameSong;
+            pibAvtor = newPIBAvtor;
+            composer = newComposer;
+            year = newYear;
+            songers = newSongers;
+        }
+        public static void SearchSongByName(string nameSong)
+        {
+            foreach (var song in songs)
+            {
+                if (song.NameSong == nameSong)
+                {
+                    Console.WriteLine(song.ToString());
+                    return;
+                }
+            }
+            Console.WriteLine("Song not found.");
+        }
+        public static void SearchSongByYear(int year)
+        {
+            foreach (var song in songs)
+            {
+                if (song.Year == year)
+                {
+                    Console.WriteLine(song.ToString());
+                    return;
+                }
+            }
+            Console.WriteLine("Song not found.");
+        }
+        public static void SaveSongsToFile(Song[] songs)
+        {
+            using (StreamWriter writer = new StreamWriter("saveSongs.txt"))
+            {
+                foreach (var song in songs)
+                {
+                    writer.WriteLine(song.ToString());
+                }
+            }
+        }
+        public static void LoadSongsFromFile(Song[] songs)
+        {
+            using (StreamReader reader = new StreamReader("loadSongs.txt"))
+            {
+                string lines;
+                while ((lines = reader.ReadLine()) != null)
+                {
+                    string[] parts = lines.Split(',');
+                    if (parts.Length >= 4)
+                    {
+                        string nameSong = parts[0].Split(':')[1].Trim();
+                        string pibAvtor = parts[1].Split(':')[1].Trim();
+                        string composer = parts[2].Split(':')[1].Trim();
+                        int year = int.Parse(parts[3].Split(':')[1].Trim());
+                        AddSong(new Song(nameSong, pibAvtor, composer, year, null));
+                    }
+                }
+            }
+        }
+        public static void FindSongsBySongerPIB(Song[] songs, string searchPIB)
+        {
+            foreach (var song in songs)
+            {
+                foreach (var songer in song.Songers)
+                {
+                    if (songer.pibSonger.Equals(searchPIB))
+                    {
+                        Console.WriteLine($"Songer {searchPIB} sing song: {song.NameSong}");
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Lab_9/Lab_9(3)/Lab_9(3)/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Lab_9_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song1 = Create.GetRandomSong();
+            Song song2 = Create.GetRandomSong();
+            Song song3 = Create.GetRandomSong();
+            Song[] songs1 = { song1, song2, song3 };
+            Song[] songs2 = new Song[0];
+            foreach (var song in songs1)
+            {
+                Console.WriteLine(song.ToString());
+            }
+            Song song4 = Create.GetRandomSong();
+            MethodsForSongs.AddSong(song4);
+            Console.WriteLine("After adding a new song:");
+            foreach (var song in songs1)
+            {
+                Console.WriteLine(song.ToString());
+            }
+            MethodsForSongs.DeleteSong(song4);
+            Console.WriteLine("After deleting the song:");
+            foreach (var song in songs1)
+            {
+                Console.WriteLine(song.ToString());
+            }
+            MethodsForSongs.SearchSongByName(song1.NameSong);
+            MethodsForSongs.SearchSongByYear(song2.Year);
+            foreach (var song in songs1)
+            {
+                Console.WriteLine(song.ToString());
+            }
+            if (song1.Songers.Length > 0)
+            {
+                MethodsForSongs.FindSongsBySongerPIB(songs1, song1.Songers[0].pibSonger);
+            }
+            MethodsForSongs.SaveSongsToFile(songs1);
+            MethodsForSongs.LoadSongsFromFile(songs2);
+            foreach (var song in songs2)
+            {
+                Console.WriteLine(song.ToString());
+            }
+
+        }
+    }
+}
